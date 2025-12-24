1882e49e83b43f9170a1d8be4d849dea0a98d6eb
Richie Nguyen <rngyen@uiowa.edu>
2025-12-24 10:09:34 -0600

    set up basic loop to test window

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ int main() {
 	cout << "Opening Snake Game Window..." << endl;
 
 	InitWindow(800, 600, "Snake Game"); //Creates the window for our game
+	SetTargetFPS(60); //Sets the target FPS for our game
+
+	while (WindowShouldClose() == false) { //Game Loop
+
+		BeginDrawing();
+
+		EndDrawing();
+
+	}
 
 	CloseWindow(); // Closes the window for our game
 
