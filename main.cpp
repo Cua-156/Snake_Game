a945d8e899a5c22e8de0bde864f2614c0dbd12a7
Richie Nguyen <rngyen@uiowa.edu>
2025-12-24 13:11:36 -0600

    Added border and text for title and score

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ Color darkGreen = { 40,50,25,255 };
 int cellSize = 30;
 int cellCount = 25;
 
+int offset = 75;
+
 double lastUpdateTime = 0;
 
 bool eventTriggered(double interval) {
@@ -43,7 +45,7 @@ class Snake {
 				float y = part.y;
 				//cout << "x is" << x << " y is " << y << endl;
 				//DrawRectangle(x*cellSize, y*cellSize, cellSize, cellSize, darkGreen);
-				Rectangle rec = { x * cellSize, y * cellSize, (float)cellSize, (float)cellSize };
+				Rectangle rec = { offset + x * cellSize, offset + y * cellSize, (float)cellSize, (float)cellSize };
 				DrawRectangleRounded(rec, 0.5, 6, darkGreen);
 			}
 		}
@@ -89,7 +91,7 @@ class Food {
 		void Draw() {
 			//Draw food here
 			// center the circle inside the grid cell
-			DrawCircle(pos.x * cellSize + cellSize/2.0f, pos.y * cellSize + cellSize/2.0f, cellSize/2, RED);
+			DrawCircle(offset + pos.x * cellSize + cellSize/2.0f, offset + pos.y * cellSize + cellSize/2.0f, cellSize/2, RED);
 			//DrawTexture(texture, pos.x*cellSize, pos.y*cellSize); // Apple is too large will not be loading any images
 		}
 
@@ -116,6 +118,7 @@ class Game {
 		Snake snake = Snake();
 		Food food = Food(snake.body);
 		bool running = true;
+		int score = 0;
 
 		void Draw() {
 			food.Draw();
@@ -124,9 +127,10 @@ class Game {
 
 		void Update() {
 			if (running) {
-			snake.Update();
-			CheckCollisionFood();
-			CheckCollisionWall();
+				snake.Update();
+				CheckCollisionFood();
+				CheckCollisionWall();
+				CheckCollisionSelf();
 			}
 		}
 
@@ -134,9 +138,10 @@ class Game {
 		void CheckCollisionFood() {
 			//Check collision between snake and food
 			if (Vector2Equals(snake.body[0], food.pos)){
-				cout << "Eating Food" << endl;
+				//cout << "Eating Food" << endl;
 				food.pos = food.randPos(snake.body);
 				snake.addSeg = true;
+				score++;
 			}
 		}
 
@@ -147,18 +152,28 @@ class Game {
 			}
 		}
 
+		void CheckCollisionSelf() {
+			//Check collision between snake and itself
+			for (int i = 1; i < snake.body.size(); i++) {
+				if (Vector2Equals(snake.body[0], snake.body[i])) {
+					GameOver();
+				}
+			}
+		}
+
 		void GameOver() {
 			cout << "Game Over!" << endl;
 			snake.Reset();
 			food.pos = food.randPos(snake.body);
 			running = false;
+			score = 0;
 		}
 };
 
 int main() {
 	cout << "Opening Snake Game Window..." << endl;
 
-	InitWindow(cellSize*cellCount, cellSize * cellCount, "Snake Game"); //Creates the window for our game
+	InitWindow(2*offset + (cellSize*cellCount), 2*offset + (cellSize * cellCount), "Snake Game"); //Creates the window for our game
 	SetTargetFPS(60); //Sets the target FPS for our game
 
 	Game game = Game();
@@ -170,7 +185,7 @@ int main() {
 		//Drawing screen
 		ClearBackground(green);
 
-		if(eventTriggered(0.2)){
+		if (eventTriggered(0.2)) {
 			//cout << "Updating Snake Position..." << endl;
 			game.Update();
 		}
@@ -187,12 +202,15 @@ int main() {
 			game.snake.direction = { -1,0 };
 			game.running = true;
 		}
-		if ((IsKeyPressed(KEY_RIGHT) ||IsKeyPressed(KEY_D))&& game.snake.direction.x != -1) {
+		if ((IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) && game.snake.direction.x != -1) {
 			game.snake.direction = { 1,0 };
 			game.running = true;
 		}
 
-		ClearBackground(green);
+		ClearBackground(GRAY);
+		DrawRectangle((float)offset - 5, (float)offset - 5, (float)cellSize * cellCount + 10, cellSize * cellCount + 10, green);
+		DrawText("Snake Game", offset - 5, 20, 40, BLACK);
+		DrawText(TextFormat("%i", game.score), cellSize * cellCount - 5 , 20, 40, BLACK);
 		game.Draw();
 
 		EndDrawing();
